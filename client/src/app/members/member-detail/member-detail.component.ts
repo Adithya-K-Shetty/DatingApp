b7eb1b9dc72543f6aaa09c3fba2ffc22ba692e6e
Adithya K Shetty <adithyakshetty01@gmail.com>
2023-05-31 23:21:19 +0530

    Added User Photo Gallery

--- client/src/app/members/member-detail/member-detail.component.ts
+++ client/src/app/members/member-detail/member-detail.component.ts
@@ -1,5 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import {
+  NgxGalleryAnimation,
+  NgxGalleryImage,
+  NgxGalleryOptions,
+} from '@kolkov/ngx-gallery';
 import { Member } from 'src/app/_models/member';
 import { MembersService } from 'src/app/_services/members.service';
 
@@ -10,6 +15,8 @@ import { MembersService } from 'src/app/_services/members.service';
 })
 export class MemberDetailComponent implements OnInit {
   member: Member | undefined;
+  galleryOptions: NgxGalleryOptions[] = [];
+  galleryImages: NgxGalleryImage[] = [];
   //creates a activated route to this component
   constructor(
     private memberService: MembersService,
@@ -18,14 +25,48 @@ export class MemberDetailComponent implements OnInit {
 
   ngOnInit(): void {
     this.loadMember();
+    this.galleryOptions = [
+      {
+        width: '500px',
+        height: '500px',
+        imagePercent: 100,
+        thumbnailsColumns: 4,
+        imageAnimation: NgxGalleryAnimation.Slide,
+        preview: false,
+      },
+    ];
+
+    //below method might be called before loadMember
+    //which might return empty array
+    //thats why add it in loadMember
+    // ---------------this.galleryImages = this.getImages();
+  }
+  getImages() {
+    if (!this.member) {
+      return [];
+    }
+
+    const imageUrls = [];
+    for (const photo of this.member.photos) {
+      imageUrls.push({
+        small: photo.url,
+        medium: photo.url,
+        big: photo.url,
+      });
+    }
+    return imageUrls;
   }
 
   loadMember() {
     //paramMap contains an array of route parameter
+    //snapshot refers to state of route in a particular time
     const username = this.route.snapshot.paramMap.get('username');
     if (!username) return;
     this.memberService.getMember(username).subscribe({
-      next: (member) => (this.member = member),
+      next: (member) => {
+        this.member = member;
+        this.galleryImages = this.getImages();
+      },
     });
   }
 }
