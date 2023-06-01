a833b7937c6744005b9a6254c1d3b59e0bb560cc
Adithya K Shetty <adithyakshetty01@gmail.com>
2023-06-01 17:59:38 +0530

    created services for uploading images to cloudinry

--- API/Extensions/ApplicationServiceExtensions.cs
+++ API/Extensions/ApplicationServiceExtensions.cs
@@ -1,4 +1,5 @@
 using API.Data;
+using API.Helpers;
 using API.interfaces;
 using API.Services;
 using Microsoft.EntityFrameworkCore;
@@ -22,6 +23,10 @@ namespace API.Extensions
             services.AddScoped<IUserRepository,UserRepository>();
 
             services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+
+            services.Configure<CloudinarySettings>(configuration.GetSection("CloudinarySettings"));
+
+            services.AddScoped<IPhotoService,PhotoService>();
             return services;
         }
     }
